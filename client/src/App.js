5e297bc1c5f1590e27df31c5e89284a205aadf02
vrich88 <mvrichards@icloud.com>
2023-08-25 14:42:46 -0500

    add tailwind basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+
+// direct components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// complete pages
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/*  */}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
